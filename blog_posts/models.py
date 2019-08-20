31d1696efa644b298575c3a8b777e16afe88a0ba
fvonstange <freddie71010@gmail.com>
2019-08-19 21:48:17 -0400

    Update model and view

--- blog_posts/models.py
+++ blog_posts/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 
 class BlogPost(models.Model):
+    # id = models.IntegerField() # pk
     title = models.TextField()
-    slug = models.SlugField()  # hello world -> hello-world
+    slug = models.SlugField(unique=True)  # hello world -> hello-world
     content = models.TextField(null=True, blank=True)
 
     def __str__(self):
--- blog_posts/views.py
+++ blog_posts/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 
 from .models import BlogPost
@@ -5,8 +6,9 @@ from .models import BlogPost
 
 def blog_post_detail(request, slug):
     queryset = BlogPost.objects.filter(slug=slug)
-    if queryset.count() >= 1:
-        obj = queryset.first()
+    if queryset.count() == 0:
+        raise Http404
+    obj = queryset.first()
     # queryset = get_object_or_404(BlogPost, slug=slug)
     template_name = 'blog_post_detail.html'
     context = {'object': obj}
