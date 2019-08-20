d2155eaa0b03ac500124a38ce06dcc7183bd985a
fvonstange <freddie71010@gmail.com>
2019-08-19 22:05:52 -0400

    Update blog_posts views to include CRUD views

--- blog_posts/views.py
+++ blog_posts/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import BlogPost
 
@@ -11,5 +11,41 @@ def blog_post_detail(request, slug):
     obj = queryset.first()
     # queryset = get_object_or_404(BlogPost, slug=slug)
     template_name = 'blog_post_detail.html'
+    context = {'object': obj}
+    return render(request, template_name, context)
+
+
+def blog_post_list_view(request):
+    # list out objects, could also be a search view
+    qs = BlogPost.objects.all()
+    template_name = 'blog_post_list.html'
+    context = {'object_list': qs}
+    return render(request, template_name, context)
+
+
+def blog_post_create_view(request):
+    template_name = 'blog_post_create.html'
+    context = {'form': None}
+    return render(request, template_name, context)
+
+
+def blog_post_detail_view(request, slug):
+    # Returns 1 object or a detail view
+    obj = get_object_or_404(BlogPost, slug=slug)
+    template_name = 'blog_post_detail.html'
+    context = {'object': obj}
+    return render(request, template_name, context)
+
+
+def blog_post_update_view(request, slug):
+    obj = get_object_or_404(BlogPost, slug=slug)
+    template_name = 'blog_post_update.html'
+    context = {'object': obj, 'form': None}
+    return render(request, template_name, context)
+
+
+def blog_post_delete_view(request, slug):
+    obj = get_object_or_404(BlogPost, slug=slug)
+    template_name = 'blog_post_delete.html'
     context = {'object': obj}
     return render(request, template_name, context)
