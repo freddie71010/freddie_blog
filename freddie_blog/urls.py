2c1565bf39427cfb1989d4676b117560cba14660
fvonstange <freddie71010@gmail.com>
2019-08-20 21:29:25 -0400

    separate out  'blog' urls into it's own urls file

--- freddie_blog/urls.py
+++ freddie_blog/urls.py
@@ -13,25 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from blog_posts.views import (
-    blog_post_create_view,
-    blog_post_delete_view,
-    blog_post_detail_view,
-    blog_post_list_view,
-    blog_post_update_view,
-)
+from blog_posts.views import blog_post_create_view
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 from .views import about_page, contact_page, example_page, home_page
 
 
 urlpatterns = [
     path('', home_page, name='home'),
-    path('blog/', blog_post_list_view),
-    path('blog/<str:slug>/', blog_post_detail_view),
-    path('blog/<str:slug>/edit', blog_post_update_view),
-    path('blog/<str:slug>/delete', blog_post_delete_view),
+    path('blog/', include('blog_posts.urls')),
     path('blog-new/', blog_post_create_view),
     # These two are the same thing:
     #       path('blog/<int: post_id>/', blog_post_detail,
