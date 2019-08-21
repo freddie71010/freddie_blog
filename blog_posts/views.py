0855a123099a222b1ed5af3dd15aa232f5b0416c
fvonstange <freddie71010@gmail.com>
2019-08-20 22:54:10 -0400

    Move template files around and create a django form for Contact page

--- blog_posts/views.py
+++ blog_posts/views.py
@@ -6,13 +6,13 @@ from .models import BlogPost
 def blog_post_list_view(request):
     # list out objects, could also be a search view
     qs = BlogPost.objects.all()
-    template_name = 'blog_post_list.html'
+    template_name = 'blog_posts/list.html'
     context = {'object_list': qs}
     return render(request, template_name, context)
 
 
 def blog_post_create_view(request):
-    template_name = 'blog_post_create.html'
+    template_name = 'blog_posts/create.html'
     context = {'form': None}
     return render(request, template_name, context)
 
@@ -20,20 +20,20 @@ def blog_post_create_view(request):
 def blog_post_detail_view(request, slug):
     # Returns 1 object or a detail view
     obj = get_object_or_404(BlogPost, slug=slug)
-    template_name = 'blog_post_detail.html'
+    template_name = 'blog_posts/detail.html'
     context = {'object': obj}
     return render(request, template_name, context)
 
 
 def blog_post_update_view(request, slug):
     obj = get_object_or_404(BlogPost, slug=slug)
-    template_name = 'blog_post_update.html'
+    template_name = 'blog_posts/update.html'
     context = {'object': obj, 'form': None}
     return render(request, template_name, context)
 
 
 def blog_post_delete_view(request, slug):
     obj = get_object_or_404(BlogPost, slug=slug)
-    template_name = 'blog_post_delete.html'
+    template_name = 'blog_posts/delete.html'
     context = {'object': obj}
     return render(request, template_name, context)
--- freddie_blog/views.py
+++ freddie_blog/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.template.loader import get_template
 
+from .forms import ContactForm
+
 
 def home_page(request):
     my_title = 'Hello there...'
@@ -19,7 +21,14 @@ def about_page(request):
 
 
 def contact_page(request):
-    return render(request, 'about.html', {'title': 'Contact'})
+    form = ContactForm(request.POST or None)
+    if form.is_valid():
+        print(form.cleaned_data)
+        form = ContactForm()
+    context = {'title': 'Contact',
+               'form': form
+               }
+    return render(request, 'form.html', context)
 
 
 def example_page(request):
