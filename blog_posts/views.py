52d1d338fac869ef36e4ecda013e63eb0c7639eb
fvonstange <freddie71010@gmail.com>
2019-08-20 21:20:41 -0400

    Create new template files, update view, update urls

--- blog_posts/views.py
+++ blog_posts/views.py
@@ -1,18 +1,6 @@
-from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 
 from .models import BlogPost
-
-
-def blog_post_detail(request, slug):
-    queryset = BlogPost.objects.filter(slug=slug)
-    if queryset.count() == 0:
-        raise Http404
-    obj = queryset.first()
-    # queryset = get_object_or_404(BlogPost, slug=slug)
-    template_name = 'blog_post_detail.html'
-    context = {'object': obj}
-    return render(request, template_name, context)
 
 
 def blog_post_list_view(request):
--- freddie_blog/urls.py
+++ freddie_blog/urls.py
@@ -13,7 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from blog_posts.views import blog_post_detail
+from blog_posts.views import (
+    blog_post_create_view,
+    blog_post_delete_view,
+    blog_post_detail_view,
+    blog_post_list_view,
+    blog_post_update_view,
+)
 from django.contrib import admin
 from django.urls import path
 
@@ -22,14 +28,17 @@ from .views import about_page, contact_page, example_page, home_page
 
 urlpatterns = [
     path('', home_page, name='home'),
-    path('blog/', blog_post_detail),
-    path('blog/<str:slug>/', blog_post_detail),
+    path('blog/', blog_post_list_view),
+    path('blog/<str:slug>/', blog_post_detail_view),
+    path('blog/<str:slug>/edit', blog_post_update_view),
+    path('blog/<str:slug>/delete', blog_post_delete_view),
+    path('blog-new/', blog_post_create_view),
     # These two are the same thing:
     #       path('blog/<int: post_id>/', blog_post_detail,
     #       re_path('blog/(?P<post_id>\d+/$', blog_post_detail,
     path('contact/', contact_page, name='contact'),
     path('about/', about_page, name='about'),
     path('example/', example_page, name='example'),
-    path('fvs-admin/', admin.site.urls),
+    path('fvs-admin/', admin.site.urls)
 
 ]
