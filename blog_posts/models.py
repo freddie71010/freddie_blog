3bc61b877a881cbeb82da303c401cabbdb02398b
fvonstange <freddie71010@gmail.com>
2019-08-29 21:01:55 -0400

    Add new date fields to blog_posts model

--- blog_posts/models.py
+++ blog_posts/models.py
@@ -19,6 +19,9 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=120)
     slug = models.SlugField(unique=True)  # hello world -> hello-world
     content = models.TextField(null=True, blank=True)
+    publish_date = models.DateTimeField(auto_now=False, auto_now_add=False, null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
