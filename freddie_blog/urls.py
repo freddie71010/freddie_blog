aca44f153a6200d2b8d6eaa2c84c497483ec22d3
fvonstange <freddie71010@gmail.com>
2019-09-04 21:55:08 -0400

    Create staticfiles folder to test saving and uploading images

--- freddie_blog/urls.py
+++ freddie_blog/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from blog_posts.views import blog_post_create_view
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
@@ -33,3 +34,9 @@ urlpatterns = [
     path('fvs-admin/', admin.site.urls)
 
 ]
+
+if settings.DEBUG:
+    # test mode
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
